d23d904dffc65603d7931d27c32c58672388bf01
Roccoon <me@wanghaoyu.cn>
2020-10-18 22:22:57 +0800

    fixed http content-length panic

---
+++ timeout_writer.go
@@ -0,0 +1,70 @@
+package timeout
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Writer is a writer with memory buffer
+type Writer struct {
+	gin.ResponseWriter
+	body         *bytes.Buffer
+	headers      http.Header
+	mu           sync.Mutex
+	timeout      bool
+	wroteHeaders bool
+	code         int
+}
+
+// NewWriter will return a timeout.Writer pointer
+func NewWriter(w gin.ResponseWriter, buf *bytes.Buffer) *Writer {
+	return &Writer{ResponseWriter: w, body: buf, headers: make(http.Header)}
+}
+
+func (w *Writer) Write(data []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.timeout || w.body == nil {
+		return 0, nil
+	}
+
+	return w.body.Write(data)
+}
+
+func (w *Writer) WriteHeader(code int) {
+	checkWriteHeaderCode(code)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.timeout || w.wroteHeaders {
+		return
+	}
+	w.writeHeader(code)
+}
+
+func (w *Writer) writeHeader(code int) {
+	w.wroteHeaders = true
+	w.code = code
+}
+
+func (w *Writer) Header() http.Header {
+	return w.headers
+}
+
+func (w *Writer) WriteString(s string) (int, error) {
+	return w.Write([]byte(s))
+}
+
+// FreeBuffer will release buffer pointer
+func (w *Writer) FreeBuffer() {
+	w.body = nil
+}
+
+func checkWriteHeaderCode(code int) {
+	if code < 100 || code > 999 {
+		panic(fmt.Sprintf("invalid http status code: %d", code))
+	}
+}
